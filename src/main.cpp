b5d84192428e06f0a1775aeaf71b6c18a782918a
Marcin <terragady@gmail.com>
2020-10-12 10:48:07 +0200

    variables

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,31 @@
+// LIBs
+
 #include <Arduino.h>
 
-void setup() {
+// Constants
+const int buttonPin = 0;
+const int driverPwmPin = 0;
+const int driverDirPin = 0;
+const int driverSleepPin = 0;
+const int driverCSPin = 0;
+const int currentGain = 20;
+
+// Vars
+
+// x = 1 - X
+
+int buttonState;
+
+unsigned long currentTime = 0;
+unsigned long previousButtonTime = 0;
+unsigned long runningMotorTime = 0;
+
+void setup()
+{
   // put your setup code here, to run once:
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  currentTime = millis();
 }
