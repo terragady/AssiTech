9f25e7fdb4ffd98511d3e89e1fbce42ee1c340de
Marcin Michalik <terragady@gmail.com>
2020-11-13 21:03:43 +0100

    lkl

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,6 @@ unsigned int softStartDelayCoef = 5;                                            
 #define STOPATFAULT                                                                      //
 /////////////////////////////////////////// END ///////////////////////////////////////////
 
-
 // #define CHECKFAULT
 
 // LIBs
